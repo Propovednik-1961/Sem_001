70a0a8786023286350ce04b3ccd8975e9b75ec4f
Alexey Novichkov <alexey-mashine@yandex.ru>
2023-02-20 23:15:08 +0300

    Задача на четное и нечетное

---
+++ tuzik_004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int diji = number % 2;
+if(diji == 0)
+{
+    Console.WriteLine("Четное " );
+}
+else
+{
+    Console.WriteLine($"Нечетное,остаток{diji}");
+}
